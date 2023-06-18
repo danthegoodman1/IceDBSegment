3d5d21436aecc6bf1361c2ccb39b34cb21309b5d
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-06-18 09:15:34 -0400

    go back to ms time

--- app.py
+++ app.py
@@ -21,14 +21,14 @@ def get_partition_range(table: str, syear: int, smonth: int, sday: int, eyear: i
             'table={}/y={}/m={}/d={}'.format(table, '{}'.format(eyear).zfill(4), '{}'.format(emonth).zfill(2), '{}'.format(eday).zfill(2))]
 
 def part_segment(row: dict) -> str:
-    rowtime = datetime.fromisoformat(row['receviedAt']) if 'receivedAt' in row else datetime.now()
+    rowtime = datetime.fromisoformat(row['receivedAt'])
     # the `table=segment/` prefix makes it effectively the `segment` table
     part = 'table={}/y={}/m={}/d={}'.format(row["table"], '{}'.format(rowtime.year).zfill(4), '{}'.format(rowtime.month).zfill(2), '{}'.format(rowtime.day).zfill(2))
     return part
 
 def format_segment(row: dict) -> dict:
     final_row = {
-        "ts": row['receviedAt'] if 'receivedAt' in row else datetime.now().isoformat(),
+        "ts": datetime.fromisoformat(row['receivedAt']).timestamp()*1000, # convert to ms
         "event": "", # replaced below
         "user_id": row['userId'] if 'userId' in row else row['anonymousId'],
         "anonymous": False if 'userId' in row else True,
